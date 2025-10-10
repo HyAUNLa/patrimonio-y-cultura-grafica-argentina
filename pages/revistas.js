4ff64e46cd92b4eb2d40272762c1ee296bcc04ca
melisabeatriz <melisabeatriz@gmail.com>
2025-10-10 13:07:58 -0300

    Sección revistas - Éxito gráfico operativa

---
+++ pages/revistas.js
@@ -0,0 +1,239 @@
+const revistas = [
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1905_001.png",
+    titulo: "Éxito Gráfico",
+    anio: 1905,
+    volumen: 9,
+    numero: "Nº 1",
+    link: "hd/es/viewer?id=f1e407d8-c0ff-4114-b2e3-d0df6e5ee1bc",
+  },
+
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1906_001.png",
+    titulo: "Éxito Gráfico",
+    anio: 1906,
+    volumen: 12,
+    numero: "Nº 5",
+    link: "/hd/es/viewer?id=5cbc2bc1-73e8-4eb3-9556-62c40911106b",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1906_002.png",
+    titulo: "Éxito Gráfico",
+    anio: 1906,
+    volumen: 12,
+    numero: "Nº 6",
+    link: "hd/es/viewer?id=b4ac42b5-6d37-4357-a69c-a03a7894f398",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1906_003.png",
+    titulo: "Éxito Gráfico",
+    anio: 1906,
+    volumen: 12,
+    numero: "Nº 7",
+    link: "hd/es/viewer?id=98ad2384-ad27-4810-bdb4-38553c789644",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1906_004.png",
+    titulo: "Éxito Gráfico",
+    anio: 1906,
+    volumen: 12,
+    numero: "Nº 8",
+    link: "hd/es/viewer?id=a0d63121-781c-4913-b921-99eb467e2f2e",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1905_002.png",
+    titulo: "Éxito Gráfico",
+    anio: 1905,
+    volumen: 10,
+    numero: "Nº 2",
+    link: "hd/es/viewer?id=50bc8426-65af-41e2-a2a0-c495be3f9a44",
+  },
+
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1906_005.png",
+    titulo: "Éxito Gráfico",
+    anio: 1906,
+    volumen: 12,
+    numero: "Nº 9",
+    link: "hd/es/viewer?id=a368140b-60d5-4f95-a1ef-0a7ffd485ba8",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1906_006.png",
+    titulo: "Éxito Gráfico",
+    anio: 1906,
+    volumen: 12,
+    numero: "Nº 11",
+    link: "hd/es/viewer?id=e6c45bcf-43b1-467d-ba35-ab0baca89858",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1906_007.png",
+    titulo: "Éxito Gráfico",
+    anio: 1906,
+    volumen: 12,
+    numero: "Nº 12",
+    link: "hd/es/viewer?id=f74a7243-e2e9-42d4-a637-5151a36fc7d8",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1905_003.png",
+    titulo: "Éxito Gráfico",
+    anio: 1905,
+    volumen: 11,
+    numero: "Nº 3",
+    link: "hd/es/viewer?id=f87e0d08-f995-4024-b691-8c9781f93c4e",
+  },
+
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1906_008.png",
+    titulo: "Éxito Gráfico",
+    anio: 1906,
+    volumen: 12,
+    numero: "Nº 13",
+    link: "hd/es/viewer?id=5a3eeec6-226c-48d4-bda8-e6001ed2928a",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1906_009.png",
+    titulo: "Éxito Gráfico",
+    anio: 1906,
+    volumen: 12,
+    numero: "Nº 14",
+    link: "hd/es/viewer?id=118d2df1-9af6-4ba5-9df1-708b6d461630",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1907_001.png",
+    titulo: "Éxito Gráfico",
+    anio: 1907,
+    volumen: 11,
+    numero: "Nº 15",
+    link: "hd/es/viewer?id=3a8a3881-f2b2-4d74-99e5-7b9b6bf769f9",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1907_002.png",
+    titulo: "Éxito Gráfico",
+    anio: 1907,
+    volumen: 11,
+    numero: "Nº 16",
+    link: "hd/es/viewer?id=4144a9b3-0bd2-4975-8a22-588cef607980",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1907_003.png",
+    titulo: "Éxito Gráfico",
+    anio: 1907,
+    volumen: 12,
+    numero: "Nº 17",
+    link: "hd/es/viewer?id=7a432792-dc29-4cc0-8405-a8aa4cb00110",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1907_004.png",
+    titulo: "Éxito Gráfico",
+    anio: 1907,
+    volumen: 12,
+    numero: "Nº 18",
+    link: "hd/es/viewer?id=203ad777-1076-4770-869b-e82e0bb4eb72",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1905_004.png",
+    titulo: "Éxito Gráfico",
+    anio: 1905,
+    volumen: 12,
+    numero: "Nº 4",
+    link: "hd/es/viewer?id=2141dc40-85e5-433c-acd2-7ce555a5c770",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1907_005.png",
+    titulo: "Éxito Gráfico",
+    anio: 1907,
+    volumen: 12,
+    numero: "Nº 19",
+    link: "hd/es/viewer?id=32a3bba1-5c0a-4718-9dbd-f36eff5efc51",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1907_006.png",
+    titulo: "Éxito Gráfico",
+    anio: 1907,
+    volumen: 12,
+    numero: "Nº 20",
+    link: "hd/es/viewer?id=ac35ebe8-7d79-4b5c-aa4a-2ae3ea12c0cc",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1907_007.png",
+    titulo: "Éxito Gráfico",
+    anio: 1907,
+    volumen: 12,
+    numero: "Nº 21",
+    link: "hd/es/viewer?id=e4bfaa8e-06ed-4cda-b124-c2bf72f6136c",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1907_008.png",
+    titulo: "Éxito Gráfico",
+    anio: 1907,
+    volumen: 12,
+    numero: "Nº 22",
+    link: "hd/es/viewer?id=51e598df-6bed-4fb7-ac00-88ad68ccceef",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1907_009.png",
+    titulo: "Éxito Gráfico",
+    anio: 1907,
+    volumen: 12,
+    numero: "Nº 23",
+    link: "hd/es/viewer?id=363ee118-5339-4b6b-a575-4744fb13c0a6",
+  },
+  {
+    imagen: "../img/revistas/exito_grafico/EG-1907_010.png",
+    titulo: "Éxito Gráfico",
+    anio: 1907,
+    volumen: 12,
+    numero: "Nº 24",
+    link: "hd/es/viewer?id=6ac3be33-e079-4740-ae42-f950bfd5baae",
+  },
+];
+
+const contenedor = document.getElementById("revistas-container");
+const botones = document.querySelectorAll("#botones button");
+
+function renderRevistas(filtroAnio = null) {
+  contenedor.innerHTML = "";
+  const filtradas = filtroAnio
+    ? revistas.filter((r) => r.anio === filtroAnio)
+    : revistas;
+  filtradas.forEach((revista) => {
+    const card = document.createElement("div");
+    card.className = "revista-infocard";
+
+    const imagen = revista?.imagen || "../img/revistas/placeholder.png";
+    const titulo = revista?.titulo || "Título no disponible";
+    const anio = revista?.anio ?? "—";
+    const volumen = revista?.volumen ?? "—";
+    const numero = revista?.numero || "—";
+    const link =
+      revista?.link ||
+      "hd/es/results?parent=84769f9f-ead4-4da6-bd40-64e35aeaa6bc&t=alt-asc&s=0";
+
+    card.innerHTML = `
+            <a href="https://hemerotecadigital.bne.es/${link}" target="_blank">
+      <img src="${imagen}" alt="${titulo}">
+      </a>
+      <h3>${titulo}</h3>
+      <p>Año ${anio} - Volumen ${volumen}</p>
+      <p>${numero}</p>
+    `;
+
+    const img = card.querySelector("img");
+    img.onerror = () => {
+      img.src = "../img/revistas/placeholder.png";
+    };
+
+    contenedor.appendChild(card);
+  });
+}
+
+botones.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    botones.forEach((b) => b.classList.remove("activo"));
+    btn.classList.add("activo");
+    const anio = parseInt(btn.getAttribute("data-anio"));
+    renderRevistas(anio);
+  });
+});
+
+renderRevistas();
